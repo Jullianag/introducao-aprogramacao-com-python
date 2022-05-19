28a8f9754201d41729605f821408b6190f95cce1
jullianag <juh_g@hotmail.com>
2022-05-19 18:14:04 -0300

    começando

---
+++ Capitulo 2/ex 2.1.py
@@ -0,0 +1,10 @@
+"""
+2.1 - Converta as seguintes expressões matemáticas para que possam
+ser calculadas usando o interpretador python.
+"""
+
+a = 10 + 20 * 30
+b = 4**2 / 30
+c = (9**4 + 2) * 6 -1
+
+print(a)
